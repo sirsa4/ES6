2de2ecc000442d2087f62df1a431a9fd51341e71
sirsa4 <yaya.abbdi@gmail.com>
2022-01-09 15:46:04 +0100

    promise dom practice setup

--- async/js.js
+++ async/js.js
@@ -1,3 +1,4 @@
+//select function
 function element(select){
     const element = document.querySelector(select);
 
@@ -9,46 +10,69 @@ function element(select){
         
     
 }
+//create function
+function create(create){
+    const cr = document.createElement(create);
+    if(cr){
+        return cr;
+    }
+}
 //get headers
 
 const header1 = element('.header1');
 const header2 = element('.header2');
 const header3 = element('.header3');
+const img = [    
+    'https://images.unsplash.com/photo-1641400504445-99dc922bbe63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1511860810434-a92f84c6f01e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    'https://images.unsplash.com/photo-1625142536105-715f16197e4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+    'https://images.unsplash.com/photo-1625142536264-4488bdf6f5e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    'https://images.unsplash.com/photo-1527519135413-1e146b552e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1507025793886-e20cd4685fae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+    'https://images.unsplash.com/photo-1599487958919-b7057e6f44e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1572218899997-01b4956b63f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
+    'https://images.unsplash.com/photo-1545145311-416b4c775a4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1599489302312-453e1fd2a175?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+    'https://images.unsplash.com/photo-1565747680668-5ca437089eba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=682&q=80',
+    'https://images.unsplash.com/photo-1640682560188-a18a3a8dfd01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+    'https://images.unsplash.com/photo-1639748217730-abf25c3ea593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+];
+const randomImg = Math.floor(Math.random() * img.length);
 
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const rando = Math.floor(Math.random() * months.length);
+/* console.log(months[rando]); */
+
+//access img
+console.log(img[randomImg]);
+
+//random image
+let random = new Image();
+random.src = img[randomImg];
+
+//Img
 const body = document.body;
+const container = create('div');
+container.appendChild(random);
+body.appendChild(container);
+
+
+
+
+
 
 //get button
 const btn = element('.btn');
 
-//Async event to change colors
+//Async event to images
 btn.addEventListener('click', ()=>{
-  console.log('working');
-});
+    console.log('working');
+  });
 
 
-//promsises basics
 
-/*
-    -promise is an inbuildt javascript method which.
-    -new Promise() takes 2 paramaters which can have any name. normally called 'resolve and reject'.
-    -the first parameter is the resolve when the promise is complete, the state would 'fulfilled'.
-    -the second parameter 'reject' is when the promise is fails, the state would be rejected.
-    -.then() function is  used to get the complete promise.
-    -.catch() is used to get the error when promise fails
- */
-const firstPromise = new Promise((reslove, reject)=>{
-    const variable = true;
 
-    if(variable){
-        reslove(`${variable} is true`);
-    } else {
-        reject(`${variable} is not true at all`);
-    }
-});
 
-firstPromise.then(()=>{
-    console.log('this is working');
-}).catch(()=>{
-    console.log('this is not working');
-})
-console.log(firstPromise);
+ 
+  
